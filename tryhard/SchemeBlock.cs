37421597d91bfad15226ce981d80c5c74cd129a7
Nicholas Glushchenko <ruscoolman@gmail.com>
2016-11-16 00:55:02 +1000

    add moving blocks

--- tryhard/SchemeBlock.cs
+++ tryhard/SchemeBlock.cs
@@ -3,25 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace tryhard
-{
-    struct Point
-    {
-        public int x;
-        public int y;
-        public Point(int Ax, int Ay)
-        {
-            x = Ax;
-            y = Ay;
-        }
-    }
-
+{ 
     class SchemeBlock
     {
         //{consts}
-            private int BlockBodyWidth  = 120;
-            private int BlockBodyHeight = 60;
+            private int  BlockBodyWidth  = 120;
+            private int  BlockBodyHeight = 60;
+            private bool isMouseDown     = false;
         //{end}
 
         //{controls}
@@ -29,29 +20,50 @@ namespace tryhard
             public System.Windows.Forms.Label NameLabel;
         //{end}
 
-        public SchemeBlock(string AName, Point APosition)
+        public SchemeBlock(string AName, System.Drawing.Point APosition)
         {
             InitializeComponent(AName, APosition);
         }
 
-        private void InitializeComponent(string AName, Point APosition)
+        private void InitializeComponent(string AName, System.Drawing.Point APosition)
         {
             //{BlockBody}
                 this.BlockBody = new System.Windows.Forms.Panel();
 
-                this.BlockBody.BackColor =     System.Drawing.Color.Beige;
-                this.BlockBody.Location  = new System.Drawing.Point(APosition.x, APosition.y);
-                this.BlockBody.Size      = new System.Drawing.Size(BlockBodyWidth, BlockBodyHeight);
+                this.BlockBody.BackColor  =     System.Drawing.Color.Beige;
+                this.BlockBody.Location   = new System.Drawing.Point(APosition.X, APosition.Y);
+                this.BlockBody.Size       = new System.Drawing.Size(BlockBodyWidth, BlockBodyHeight);
+                this.BlockBody.MouseDown += new System.Windows.Forms.MouseEventHandler(this.SchemeBodyMouseDown);
+                this.BlockBody.MouseMove += new System.Windows.Forms.MouseEventHandler(this.SchemeBodyMouseMove);
+                this.BlockBody.MouseUp   += new System.Windows.Forms.MouseEventHandler(this.SchemeBodyMouseUp);
             //{end}
 
             //{NameLabel}
-                this.NameLabel = new System.Windows.Forms.Label();
+            this.NameLabel = new System.Windows.Forms.Label();
 
                 this.NameLabel.Location = new System.Drawing.Point(0, 0);
                 this.NameLabel.Text     = AName;
 
                 this.BlockBody.Controls.Add(this.NameLabel);
             //{end}
+        }
+
+        public void SchemeBodyMouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+        }
+
+        public void SchemeBodyMouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                this.BlockBody.Location = new System.Drawing.Point(e.Location.X, e.Location.Y);
+            }
+        }
+
+        public void SchemeBodyMouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
         }
     }
 }
