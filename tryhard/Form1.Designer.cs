0cd3af3a607aa389154fa653b433aaa25a2cfcd7
Nicholas Glushchenko <ruscoolman@gmail.com>
2016-11-16 00:55:34 +1000

    update UI

--- tryhard/Form1.Designer.cs
+++ tryhard/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace tryhard
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.DrawingPanel = new System.Windows.Forms.Panel();
+            this.AddBlockButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DrawingPanel
+            // 
+            this.DrawingPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.DrawingPanel.Location = new System.Drawing.Point(12, 12);
+            this.DrawingPanel.Name = "DrawingPanel";
+            this.DrawingPanel.Size = new System.Drawing.Size(882, 586);
+            this.DrawingPanel.TabIndex = 0;
+            this.DrawingPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DrawingPanel_MouseDown);
+            // 
+            // AddBlockButton
+            // 
+            this.AddBlockButton.Location = new System.Drawing.Point(900, 12);
+            this.AddBlockButton.Name = "AddBlockButton";
+            this.AddBlockButton.Size = new System.Drawing.Size(48, 48);
+            this.AddBlockButton.TabIndex = 1;
+            this.AddBlockButton.Text = "+";
+            this.AddBlockButton.UseVisualStyleBackColor = true;
+            this.AddBlockButton.Click += new System.EventHandler(this.AddBlockButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(968, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(968, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1343, 610);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AddBlockButton);
+            this.Controls.Add(this.DrawingPanel);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel DrawingPanel;
+        private System.Windows.Forms.Button AddBlockButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- tryhard/Form1.cs
+++ tryhard/Form1.cs
@@ -10,11 +10,29 @@ using System.Windows.Forms;
 
 namespace tryhard
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        private SchemeBlock[] Blocks;
+
+        public MainForm()
         {
+            Blocks = new SchemeBlock[0];
             InitializeComponent();
+        }
+
+        private void AddBlockButton_Click(object sender, EventArgs e)
+        {
+            Array.Resize(ref Blocks, Blocks.Length + 1);
+            System.Drawing.Point Pos = new System.Drawing.Point(10, 10 * Blocks.Length + 60 * Blocks.Length);
+
+            Blocks[Blocks.Length - 1] = new SchemeBlock("Труба" + (Blocks.Length - 1).ToString(), Pos);
+            this.DrawingPanel.Controls.Add(Blocks[Blocks.Length - 1].BlockBody);
+            this.label1.Text = Blocks.Length.ToString();
+        }
+
+        private void DrawingPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+
         }
     }
 }
