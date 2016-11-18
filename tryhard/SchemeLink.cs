ceee0b3f45ed6534b56522bae05f9f147b1696b2
Nicholas Glushchenko <ruscoolman@gmail.com>
2016-11-19 00:41:41 +1000

    add schemelink class

---
+++ tryhard/SchemeLink.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryhard
+{
+    class SchemeLink
+    {
+        private int InputSchemeIndex;
+        private int InputSchemePointIndex;
+        private int OutputSchemeIndex;
+        private int OutputSchemePointIndex;
+
+        public SchemeLink(int AInputSchemeIndex, int AInputSchemePointIndex,
+                          int AOutputSchemeIndex, int AOutputSchemePointIndex)
+        {
+            InputSchemeIndex       = AInputSchemeIndex;
+            InputSchemePointIndex  = AInputSchemePointIndex;
+            OutputSchemeIndex      = AOutputSchemeIndex;
+            OutputSchemePointIndex = AOutputSchemePointIndex;
+        }
+
+        public System.Drawing.Point GetInputSchemePointLocation(SchemeBlock AInputBlock)
+        {
+            return AInputBlock.BlockBody.Location; ///
+        }
+    }
+}
