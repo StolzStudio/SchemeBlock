9dfdd80cab91672cdf68db790d1e9296bd6beb9e
Nicholas Glushchenko <ruscoolman@gmail.com>
2016-11-19 00:41:01 +1000

    add point in schemeblock

--- tryhard/SchemeBlock.cs
+++ tryhard/SchemeBlock.cs
@@ -10,16 +10,18 @@ namespace tryhard
     class SchemeBlock
     {
         //{consts}
-            private int  BlockBodyWidth  = 120;
-            private int  BlockBodyHeight = 60;
-            private bool isMouseDown     = false;
+            private int  BlockBodyWidth   = 120;
+            private int  BlockBodyHeight  = 60;
+            private int  BlockPointSize   = 6;
+            private bool isMouseDown      = false;
 
             private MainForm Form;
         //{end}
 
         //{controls}
-            public System.Windows.Forms.Panel BlockBody;
-            public System.Windows.Forms.Label NameLabel;
+            public System.Windows.Forms.Panel   BlockBody;
+            public System.Windows.Forms.Label   NameLabel;
+            public System.Windows.Forms.Panel[] BlockPoints;
         //{end}
 
         public SchemeBlock(string AName, System.Drawing.Point APosition, MainForm AForm)
@@ -39,6 +41,30 @@ namespace tryhard
                 this.BlockBody.MouseDown += new System.Windows.Forms.MouseEventHandler(this.SchemeBodyMouseDown);
                 this.BlockBody.MouseMove += new System.Windows.Forms.MouseEventHandler(this.SchemeBodyMouseMove);
                 this.BlockBody.MouseUp   += new System.Windows.Forms.MouseEventHandler(this.SchemeBodyMouseUp);
+            //{end}
+
+            //{BlockPoints}
+                this.BlockPoints = new System.Windows.Forms.Panel[4];
+
+                this.BlockPoints[0]          = new System.Windows.Forms.Panel();
+                this.BlockPoints[0].Location = new System.Drawing.Point(BlockBodyWidth / 2 - BlockPointSize / 2, 0);
+
+                this.BlockPoints[1]          = new System.Windows.Forms.Panel();
+                this.BlockPoints[1].Location = new System.Drawing.Point(BlockBodyWidth - BlockPointSize, BlockBodyHeight / 2 - BlockPointSize / 2);
+
+                this.BlockPoints[2]          = new System.Windows.Forms.Panel();
+                this.BlockPoints[2].Location = new System.Drawing.Point(BlockBodyWidth / 2 - BlockPointSize / 2, BlockBodyHeight - BlockPointSize);
+
+                this.BlockPoints[3]          = new System.Windows.Forms.Panel();
+                this.BlockPoints[3].Location = new System.Drawing.Point(0, BlockBodyHeight / 2 - BlockPointSize / 2);
+
+                for (int i = 0; i < 4; i++)
+                {
+                    this.BlockPoints[i].Size = new System.Drawing.Size(BlockPointSize, BlockPointSize);
+                    this.BlockPoints[i].BackColor = System.Drawing.Color.Black;
+                    this.BlockBody.Controls.Add(this.BlockPoints[i]);
+                }
+            
             //{end}
 
             //{NameLabel}
