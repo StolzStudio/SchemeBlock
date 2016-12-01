b36fb76009d8a5f9cd80d242c91e0b9321451eb5
Nicholas Glushchenko <ruscoolman@gmail.com>
2016-12-02 02:58:10 +1000

    add CalcContainer class

---
+++ tryhard/CalcContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryhard
+{
+    class CalcContainer
+    {
+        public int    Id;
+        public string Class;
+        public int    IdFather;
+        public string ClassFather;
+
+        public CalcContainer(int AId, string AClass, int AIdFather, string AClassFather)
+        {
+            Id          = AId;
+            Class       = AClass;
+            IdFather    = AIdFather;
+            ClassFather = AClassFather;
+        }
+    }
+}
