d64f18af944b10abbd21a32294891e63652830be
DiMonster07 <territory_cech@mail.ru>
2016-12-14 20:49:43 +1000

    Add CalculationManager.cs for finally calculate

---
+++ tryhard/CalculationManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace tryhard
+{
+    class CalcBlock
+    {
+        public int FFirstIndex = 0;
+        public int FSecondIndex = 0;
+        public List<string> FFirstDBParametrs = new List<string>();
+        public List<string> FSecondDBParametrs = new List<string>();
+
+        public CalcBlock(int AFirstIndex, List<string> AFirstDBParametrs,
+                         int ASecondIndex, List<string> ASecondDBParametrs)
+        {
+
+        }
+        
+    }
+}
