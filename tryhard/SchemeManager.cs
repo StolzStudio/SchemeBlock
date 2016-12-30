713a659bd36d27a230429e0132648afffcc90e14
Nicholas Glushchenko <ruscoolman@gmail.com>
2016-12-30 12:49:15 +1000

    add invalidate form in manager

--- tryhard/SchemeManager.cs
+++ tryhard/SchemeManager.cs
@@ -83,6 +83,8 @@ namespace tryhard
             {
                 Blocks[key].ClearFocus();
             }
+
+            Form.DrawingPanel.Invalidate();
         }
 
         public void TrySetFocusInLinks(Point Coord)
