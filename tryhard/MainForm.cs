bd00d096d684aab4b5635c9ca02dd7e0d928ebee
Dmitrii <territory_cech@mail.ru>
2017-01-10 12:56:33 +1000

    Improve MainForm => add function argument

--- tryhard/MainForm.cs
+++ tryhard/MainForm.cs
@@ -124,7 +124,7 @@ namespace tryhard
                     CalcBlocks[Link.SecondBlockIndex].InputLinks.Add(Link.FirstBlockIndex);
                 }
             }
-            return CalcManager.CalculateBlocksCombinations(Meta, CalcBlocks);
+            return CalcManager.CalculateBlocksCombinations(Meta, CalcBlocks, APageType);
         }  
 
         private void CalcButton_Click(object sender, EventArgs e)
