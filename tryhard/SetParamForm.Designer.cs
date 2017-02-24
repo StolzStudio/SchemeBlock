7fae5802aaf54558b7f624b6673884d641286b93
Nicholas Glushchenko <ruscoolman@gmail.com>
2017-02-25 04:33:18 +1000

    CountManager: add new form to work with edit object params

---
+++ tryhard/SetParamForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace tryhard
+{
+    partial class SetParamForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CategoryLabel = new System.Windows.Forms.Label();
+            this.TypeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CategoryLabel
+            // 
+            this.CategoryLabel.AutoSize = true;
+            this.CategoryLabel.Location = new System.Drawing.Point(12, 18);
+            this.CategoryLabel.Name = "CategoryLabel";
+            this.CategoryLabel.Size = new System.Drawing.Size(35, 13);
+            this.CategoryLabel.TabIndex = 0;
+            this.CategoryLabel.Text = "label1";
+            // 
+            // TypeLabel
+            // 
+            this.TypeLabel.AutoSize = true;
+            this.TypeLabel.Location = new System.Drawing.Point(137, 18);
+            this.TypeLabel.Name = "TypeLabel";
+            this.TypeLabel.Size = new System.Drawing.Size(35, 13);
+            this.TypeLabel.TabIndex = 1;
+            this.TypeLabel.Text = "label1";
+            // 
+            // SetParamForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 416);
+            this.Controls.Add(this.TypeLabel);
+            this.Controls.Add(this.CategoryLabel);
+            this.Name = "SetParamForm";
+            this.Text = "SetParamForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CategoryLabel;
+        private System.Windows.Forms.Label TypeLabel;
+    }
+}
